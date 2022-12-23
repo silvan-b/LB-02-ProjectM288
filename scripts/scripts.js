a362fb845ef585af0385b9b1eced359cfe892c76
Silvan <silvan_buehlmann@gmx.ch>
2022-12-23 07:51:03 +0100

    Neues Design, Java Functions für Login Promillerechner mit neuem Design alles Work in progress

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,3 +32,13 @@ document.getElementById("submitLogin").addEventListener("click", (e) => {
     }
 })
 
+
+$('.switch label').on('click', function(){
+    var indicator = $(this).parent('.switch').find('span');
+    if ( $(this).hasClass('right') ){
+        $(indicator).addClass('right');
+    } else {
+        $(indicator).removeClass('right');
+    }
+});
+
